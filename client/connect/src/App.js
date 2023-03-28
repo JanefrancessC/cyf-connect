a514d498bc8647ad7b9e1eada60956d320c5ead9
JanefrancessC <janefrancesschioma@gmail.com>
2023-03-28 15:40:35 +0100

    fetching data from api updated

--- client/connect/src/App.js
+++ client/connect/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 import memories from './images/memories.png';
 import Posts from './components/Posts/Posts';
-// import Posts from './components/Posts/Posts';
+import { getPosts } from './actions/posts';
 import Form from './components/Form/Form';
-import useStyles from './styles'
+import useStyles from './styles';
+
 
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch])
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
--- client/connect/src/components/Posts/Posts.js
+++ client/connect/src/components/Posts/Posts.js
@@ -1,9 +1,13 @@
 import { Container } from "@material-ui/core";
 import React from "react";
+import { useSelector } from "react-redux";
 import Post from "./Post/Post";
 import useStyles from './styles'
 const Posts = () => {
+  const posts = useSelector((state) => state.posts);
   const classes = useStyles();
+
+  console.log(posts)
   return (
    <Container>
     <h1>Posts</h1>
